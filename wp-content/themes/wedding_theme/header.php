0a14c6eaf3e78bd6223e42164e9f95400923f038
Jessica Peng <jessica@oomba.com>
2017-03-04 19:40:55 -0800

    Moved around content

--- wp-content/themes/wedding_theme/header.php
+++ wp-content/themes/wedding_theme/header.php
@@ -25,56 +25,10 @@
 		<div class="container">
 			<div class="row">
 				<nav class="blog-nav">
-					<a class="blog-nav-item active" href="#">Home</a>
+					<a class="blog-nav-item active" href="/#">Home</a>
 					<?php wp_list_pages( '&title_li=' ); ?>
 				</nav>
 
 			</div>
-		</div>
-	</div>
-
-	<div class="container">
-
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		  </ol>
-
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner" role="listbox">
-		    <div class="item active">
-		      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/12/77.jpg?w=870&amp;h=580" alt="...">
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		    <div class="item">
-		      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/11/pre-reception-87.jpg?w=870&amp;h=580" alt="...">
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		    ...
-		    <div class="item">
-		      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/09/binhto-eric-wedding-finals-0498.jpg?w=682&amp;h=1024" alt="...">
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		    ...
-		  </div>
-
-		  <!-- Controls -->
-		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		    <span class="sr-only">Previous</span>
-		  </a>
-		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		    <span class="sr-only">Next</span>
-		  </a>
 		</div>
 	</div>
--- wp-content/themes/wedding_theme/index.php
+++ wp-content/themes/wedding_theme/index.php
@@ -4,13 +4,51 @@
 
 		<div class="col-sm-8 blog-main">
 
-			<?php
-			if ( have_posts() ) : while ( have_posts() ) : the_post();
+			<div class="container">
 
-				get_template_part( 'content', get_post_format() );
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				  </ol>
 
-			endwhile; endif;
-			?>
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/12/77.jpg?w=870&amp;h=580" alt="...">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/11/pre-reception-87.jpg?w=870&amp;h=580" alt="...">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    ...
+				    <div class="item">
+				      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/09/binhto-eric-wedding-finals-0498.jpg?w=682&amp;h=1024" alt="...">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    ...
+				  </div>
+
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+			</div>
 
 		</div> <!-- /.blog-main -->
 
