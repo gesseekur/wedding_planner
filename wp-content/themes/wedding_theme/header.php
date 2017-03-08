c9ba5166d4220cc816331a7342231ed3e42bd42c
Jessica Peng <jessikuhh@gmail.com>
2017-03-08 00:16:38 -0800

    Added home page and carousel!

--- wp-content/themes/wedding_theme/header.php
+++ wp-content/themes/wedding_theme/header.php
@@ -8,9 +8,10 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<?php echo get_bloginfo( 'name' ); ?>
+	<!-- <?php echo get_bloginfo( 'name' ); ?> -->
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -21,10 +22,13 @@
 
 <body>
 
-	<div class="blog-masthead">
+	<div class="blog-masthead center-block">
 		<div class="container">
+			<img class='center-block' src="" alt="Header" style='width: 800px; height: 245px; margin-top: 80px;' />
+		</div>
+		<div class="container center-block">
 			<div class="row">
-				<nav class="blog-nav">
+				<nav class="blog-nav text-center">
 					<a class="blog-nav-item active" href="/#">Home</a>
 					<?php wp_list_pages( '&title_li=' ); ?>
 				</nav>
--- wp-content/themes/wedding_theme/page-home.php
+++ wp-content/themes/wedding_theme/page-home.php
@@ -1,45 +1,108 @@
 <?php get_header(); ?>
 
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+  <div class="container">
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/12/77.jpg?w=870&amp;h=580" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
+    <!-- main slider carousel -->
+    <div class="row">
+        <div class="col-md-12" id="slider">
+
+                <div class="col-md-12" id="carousel-bounding-box">
+                    <div id="myCarousel" class="carousel slide">
+                        <!-- main slider carousel items -->
+                        <div class="carousel-inner">
+                            <div class="active item" data-slide-number="0">
+                                <img src="http://placehold.it/1200x480&amp;text=one" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="1">
+                              <img src="http://placehold.it/1200x480/888/FFF" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="2">
+                                <img src="http://placehold.it/1200x480&amp;text=three" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="3">
+                                <img src="http://placehold.it/1200x480&amp;text=four" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="4">
+                                <img src="http://placehold.it/1200x480&amp;text=five" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="5">
+                                <img src="http://placehold.it/1200x480&amp;text=six" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="6">
+                                <img src="http://placehold.it/1200x480&amp;text=seven" class="img-responsive">
+                            </div>
+                            <div class="item" data-slide-number="7">
+                                <img src="http://placehold.it/1200x480&amp;text=eight" class="img-responsive">
+                            </div>
+                        </div>
+                        <!-- main slider carousel nav controls -->
+                        <!-- <a class="carousel-control left" href="#myCarousel" data-slide="prev">‹</a> -->
+
+                        <!-- <a class="carousel-control right" href="#myCarousel" data-slide="next">›</a> -->
+                    </div>
+                </div>
+
+        </div>
+
+
+        <!-- thumb navigation carousel -->
+        <div class="col-md-12 hidden-sm hidden-xs" id="slider-thumbs">
+
+            <!-- thumb navigation carousel items -->
+          <ul class="list-inline carousel-thumbnails">
+          <li> <a id="carousel-selector-0" class="selected">
+            <img src="http://placehold.it/80x60&amp;text=one" class="img-responsive">
+          </a></li>
+          <li> <a id="carousel-selector-1">
+            <img src="http://placehold.it/80x60&amp;text=two" class="img-responsive">
+          </a></li>
+          <li> <a id="carousel-selector-2">
+            <img src="http://placehold.it/80x60&amp;text=three" class="img-responsive">
+          </a></li>
+          <li> <a id="carousel-selector-3">
+            <img src="http://placehold.it/80x60&amp;text=four" class="img-responsive">
+          </a></li>
+                <li> <a id="carousel-selector-4">
+            <img src="http://placehold.it/80x60&amp;text=five" class="img-responsive">
+          </a></li>
+          <li> <a id="carousel-selector-5">
+            <img src="http://placehold.it/80x60&amp;text=six" class="img-responsive">
+          </a></li>
+          <li> <a id="carousel-selector-6">
+            <img src="http://placehold.it/80x60&amp;text=seven" class="img-responsive">
+          </a></li>
+          <li> <a id="carousel-selector-7">
+            <img src="http://placehold.it/80x60&amp;text=eight" class="img-responsive">
+          </a></li>
+            </ul>
+
+        </div>
     </div>
-    <div class="item">
-      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/11/pre-reception-87.jpg?w=870&amp;h=580" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
-    <div class="item">
-      <img src="https://kellydupratweddingplanner.files.wordpress.com/2016/09/binhto-eric-wedding-finals-0498.jpg?w=682&amp;h=1024" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
+    <!--/main slider carousel-->
+
   </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
 <?php get_footer(); ?>
+
+<script type="text/javascript">
+  $('#myCarousel').carousel({
+    interval: 4000
+  });
+
+  // handles the carousel thumbnails
+  $('[id^=carousel-selector-]').click( function(){
+    var id_selector = $(this).attr("id");
+    var id = id_selector.substr(id_selector.length -1);
+    id = parseInt(id);
+  $('#myCarousel').carousel(id);
+  $('[id^=carousel-selector-]').removeClass('selected');
+  $(this).addClass('selected');
+  });
+
+  // when the carousel slides, auto update
+  $('#myCarousel').on('slid', function (e) {
+    var id = $('.item.active').data('slide-number');
+    id = parseInt(id);
+  $('[id^=carousel-selector-]').removeClass('selected');
+  $('[id=carousel-selector-'+id+']').addClass('selected');
+  });
+</script>
