90744a31bcac8a7d7f9a3131dd0f6b072afa3544
Jessica Peng <jessikuhh@gmail.com>
2017-03-13 23:17:53 -0700

    Added sweet notes

--- wp-content/themes/wedding_theme/page-about.php
+++ wp-content/themes/wedding_theme/page-about.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
-<h1> Hi</h1>
 
+<div class="container ">
+  <div class="col-md-10 col-md-offset-1 text-center sweet-notes">
+    <!-- <p class='.sweet-notes'> -->
+      <?php
+        echo apply_filters('the_content', get_post_field('post_content', $id));
+      ?>
+    <!-- </p> -->
+  </div>
+
+</div>
 
 <?php get_footer(); ?>
